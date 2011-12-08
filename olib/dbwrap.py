e0743410ecc4a36510e1e10558f14f0d616462b7
Oleg Pudeyev <oleg@bsdpower.com>
2011-12-08 03:45:38 +0000

    Expand our postgres wrapper

--- olib/dbwrap.py
+++ olib/dbwrap.py
@@ -2,6 +2,24 @@ import psycopg2
 
 from . import dtuple
 
+class ExpressionValue(str):
+    pass
+
+def quote_expression_value(value):
+    return value
+
+psycopg2.extensions.adapters[ExpressionValue] = quote_expression_value
+
+# Quoting of table/schema names in psycopg2:
+# http://osdir.com/ml/python.db.psycopg.devel/2004-10/msg00012.html
+class SchemaName(str):
+    pass
+
+def quote_schema_name(value):
+    return '"' + value.replace('"', '""') + '"'
+
+psycopg2.extensions.adapters[SchemaName] = quote_schema_name
+
 class CursorWrapper:
     def __init__(self, cursor, conn):
         self.cursor = cursor
@@ -33,8 +51,23 @@ class CursorWrapper:
             if sql:
                 self.execute(sql)
     
-    def fetchall(self):
-        return self.cursor.fetchall()
+    #def fetchall(self):
+        #return self.cursor.fetchall()
+    
+    # Higher-level interface
+    
+    def one(self, sql, args=None):
+        self.execute(sql, args)
+        desc = dtuple.TupleDescriptor(self.cursor.description)
+        row = self.cursor.fetch()
+        row = dtuple.DatabaseTuple(desc, row)
+        return row
+    
+    def one_check(self, sql, args=None):
+        row = self.one(sql, args)
+        if row is None:
+            raise "No data"
+        return row
     
     def all(self, sql, args=None):
         self.execute(sql, args)
@@ -42,6 +75,23 @@ class CursorWrapper:
         rows = self.cursor.fetchall()
         rows = [dtuple.DatabaseTuple(desc, row) for row in rows]
         return rows
+    
+    def one_value(self, sql, args=None):
+        self.execute(sql, args)
+        desc = dtuple.TupleDescriptor(self.cursor.description)
+        row = self.cursor.fetch()
+        if row is None:
+            raise "No data"
+        return row[0]
+    
+    def all_values(self, sql, args=None):
+        self.execute(sql, args)
+        desc = dtuple.TupleDescriptor(self.cursor.description)
+        rows = self.cursor.fetchall()
+        rows = [row[0] for row in rows]
+        return rows
+    
+    # Transactions
     
     def begin(self):
         self._transaction_depth_request += 1
@@ -56,6 +106,50 @@ class CursorWrapper:
     
     def close(self):
         return self.cursor.close()
+    
+    # Statements
+    
+    #def insert_dict(self, table, dict):
+    #    table = quote_name(table)
+    #    keys_str = ','.join(map(quote_name, dict.keys()))
+    #    values = []
+    #    values_placeholders= []
+    #    for value in dict.values():
+    #        if isinstance(value, ExpressionValue):
+    #            values_placeholders.append(value)
+    #        else:
+    #            values_placeholders.append('%s')
+    #            values.append(value)
+    #    values_str = ','.join(values_placeholders)
+    #    sql = 'insert into %s (%s) values (%s)' % (table, keys_str, values_str)
+    #    self.execute(sql, values)
+    
+    def insert_dict(self, table, dict):
+        placeholders = '%s' * len(dict)
+        sql = 'insert into %%s (%s) values (%s)' % (placeholders, placeholders)
+        table = SchemaName(table)
+        columns = [SchemaName(column) for column in dict.keys()]
+        values = dict.values()
+        self.execute(sql, (table, columns, values))
+    
+    # DDL statements
+    
+    def add_fkey(self, table, column, target_table=None, target_column=None):
+        target_table = column[:-3] + 's'
+        target_column = 'id'
+        name = '%s_%s_fk' % (table, column)
+        vars = {
+            'table': table,
+            'column': column,
+            'name': name,
+            'target_table': target_table,
+            'target_column': target_column,
+        }
+        self.execute('''
+            alter table %(table)s add constraint %(name)s
+                foreign key (%(column)s)
+                references %(target_table)s (%(target_column)s)
+        ''' % vars)
 
 class CursorContextManager:
     def __init__(self, cursor):
@@ -103,3 +197,6 @@ class ConnectionWrapper:
     
     def reconnect(self):
         self.connect()
+    
+    def expr(self, value):
+        return ExpressionValue(self)
