8bd2c6ff27cedf75b6e2518c06e40faf4e0a0b83
Oleg Pudeyev <oleg@bsdpower.com>
2013-08-27 16:48:37 -0400

    setup.py for pip

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='olib',
+    version='0.1',
+    description='Things & Stuff',
+    author='Oleg Pudeyev',
+    author_email='oleg@bsdpower.com',
+    url='http://github.com/p/olib',
+    packages=['olib'],
+)
